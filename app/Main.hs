c751f68b15cdd151c376227c29bb55a2bb07c3b2
Johan Hidding <j.hidding@esciencecenter.nl>
2018-09-01 14:31:07 +0200

    remove all things glossy

--- app/Main.hs
+++ app/Main.hs
@@ -4,26 +4,7 @@ import Controller
 import Model
 import View
 
-import Graphics.Gloss
 import Game
-import PaintBoard
-
-import Graphics.Gloss.Interface.IO.Game
 
 main :: IO ()
-main = playIO (InWindow "Counter" (1000, 850) (10, 10)) -- Or FullScreen
-              black            -- Background color
-              30               -- Frames per second
-              initialState     -- Initial state
-              view             -- View function
-              input            -- Event function
-              step             -- Step function 
-
--- main :: IO ()
--- main = do
---     let (path, c) = fireRay classicBoard $ forward (Ray (9, 0) North)
---     p <- paintBoard classicBoard
---     let p' = pictures [p, color magenta (paintPath $ (9, 0) : path)]
---     putStrLn $ "computed path: " ++ show path ++ " and " ++ show c
---     display (InWindow "Khet" (1000, 800) (100, 100))
---             black p'
+main = putStrLn "Nothing here yet!"
--- src/Controller.hs
+++ src/Controller.hs
@@ -5,20 +5,7 @@ module Controller where
 import Model
 import Game
 import qualified Data.Map.Strict as Map
-
-import Graphics.Gloss
-import Graphics.Gloss.Interface.IO.Game
-import System.Random
-
 -- | Handle one iteration of the game
-step :: Float -> GameState -> IO GameState
-step secs = return 
-
--- | Handle user input
-input :: Event -> GameState -> IO GameState
-input e gstate = do
-    print e
-    return (inputKey e gstate)
 
 moveSelection :: Orientation -> Position -> Position
 moveSelection North (x, y) = (x, (y + 1) `min` 7)
@@ -29,15 +16,3 @@ moveSelection East  (x, y) = ((x + 1) `min` 9, y)
 updateSelection :: (Position -> Position) -> GameState -> GameState
 updateSelection f s@GameState{selection=sel} = s { selection = f sel }
 
-keyAction :: SpecialKey -> GameState -> GameState
-keyAction KeyUp    = updateSelection (moveSelection North)
-keyAction KeyLeft  = updateSelection (moveSelection West) 
-keyAction KeyDown  = updateSelection (moveSelection South)
-keyAction KeyRight = updateSelection (moveSelection East) 
-keyAction KeyEnter = cyclePhase
-keyAction _ = id
-
-inputKey :: Event-> GameState -> GameState
-inputKey (EventKey (SpecialKey k) Down _ _) gstate
-  = keyAction k gstate
-inputKey _ gstate = gstate -- Otherwise keep the same
